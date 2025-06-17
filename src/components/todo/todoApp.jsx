36826437656db2bbb2ea75abe2a7a50ef21111cc
B21DCCN712 <toannobi2k3@gmail.com>
2025-06-18 00:52:01 +0700

    cấu hình router, setup layout

---
+++ src/components/todo/todoApp.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./todo.css";
+import TodoData from "./todoData";
+import TodoNew from "./todoNew";
+import imgLogo from "../../assets/react.svg";
+
+const TodoApp = () => {
+    const [todoList, setTodoList] = useState([]);
+
+    const addNewToDo = (name) => {
+        if (name != "") {
+            const newTodo = {
+                id: randomIntFromInterval(1, 1000),
+                name: name,
+            };
+            setTodoList([...todoList, newTodo]);
+        }
+    };
+
+    const deleteToDo = (id) => {
+        console.log("Deleteid : ", id);
+        const newTodo = todoList.filter((item) => item.id != id);
+        setTodoList(newTodo);
+    };
+
+    const randomIntFromInterval = (min, max) => {
+        // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    return (
+        <div className="todo-container">
+            <div className="todo-title">Todo List</div>
+            <TodoNew addNewToDo={addNewToDo} />
+
+            {todoList.length > 0 ? (
+                <TodoData todoList={todoList} deleteToDo={deleteToDo} />
+            ) : (
+                <div className="Image">
+                    <img src={imgLogo} className="logo" alt="" />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default TodoApp;
